9adc3ad73822a03681037a6aa7d0f13a85ea4ac7
RybenLigst <91519082+RybenLigst@users.noreply.github.com>
2024-09-24 19:24:44 +0300

    Delete ranch directory

--- ranch/rancher_custom/custom-scripts/modification-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Приклад: Тимчасове вимкнення самовідновлення
-kubectl patch deployment rancher -n cattle-system --type='json' -p='[{"op": "replace", "path": "/spec/replicas", "value": 0}]'
-
