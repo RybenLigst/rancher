188baaad52d05a34105821879d5d89dd0ad09a7a
RybenLigst <91519082+RybenLigst@users.noreply.github.com>
2024-09-24 19:59:42 +0300

    Delete ra directory

--- ra/generate.go
+++
@@ -1,5 +0,0 @@
-//go:generate go run pkg/codegen/buildconfig/writer.go pkg/codegen/buildconfig/main.go
-//go:generate go run pkg/codegen/generator/cleanup/main.go
-//go:generate go run pkg/codegen/main.go
-//go:generate scripts/build-crds
-package main
